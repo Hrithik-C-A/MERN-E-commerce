4494cc902836b4186c7feed984b0772500829c9b
Hrithik C A <hrithikca@gmail.com>
2023-09-02 11:45:00 +0530

    added middleware asyncHandler which handled async requests...added productRoutes.js which has / and /:id requests and in server.js added app.use(/api/products, productRoutes)

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import cors from 'cors';
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js';
 
 const port = process.env.PORT || 5000;
 
@@ -16,15 +16,7 @@ app.get('/',(req, res)=>{
     res.send('Api is running...')
 });
 
-app.get('/api/products',(req, res)=>{
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res)=>{
-    const productId = req.params.id;
-    const product = products.find((p)=> p._id === productId);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port,()=>{
     console.log(`Server is running on port: ${port}`);
