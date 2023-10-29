f0ac8338b3a5e5b96885aedf5bc1c751b2bb63c5
Hrithik C A <hrithikca@gmail.com>
2023-10-29 11:57:35 +0530

    bug fixes

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -9,10 +9,10 @@ const errorHandler = (err, req, res, next)=>{
     let message = err.message;
 
     //Check for mongoose bad ObjectId
-    if(err.name === 'CastError' && err.kind === 'ObjectId'){
-        message = 'Resource Not Found';
-        statusCode = 404
-    }
+    // if(err.name === 'CastError' && err.kind === 'ObjectId'){
+    //     message = 'Resource Not Found';
+    //     statusCode = 404
+    // }
 
     res.status(statusCode).json({
         message,
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { getProductById, getProducts, createProduct, updateProduct, deleteProduct, createProductReview, getTopProducts } from "../controllers/productController.js";
-import { protect, admin } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js';
+import checkObjectId from "../middleware/checkObjectId.js";
 
 const router = express.Router();
 
@@ -8,8 +9,8 @@ router.route('/').get( getProducts).post(protect, admin, createProduct);
 
 router.route('/top').get(getTopProducts);
 
-router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router.route('/:id').get(checkObjectId, getProductById).put(protect, admin, checkObjectId, updateProduct).delete(protect, admin, checkObjectId, deleteProduct);
 
-router.route('/:id/reviews').post(protect, createProductReview);
+router.route('/:id/reviews').post(protect,checkObjectId, createProductReview);
 
 export default router;
